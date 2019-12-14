c9a859bd81227f292ba274b082ea23b47ac6dbef
amarcova90 <amarcova@stanford.edu>
2019-12-14 12:36:47 -0800

    aded electron current ion velocity fluctuations

--- main.cpp
+++ main.cpp
@@ -3,7 +3,6 @@
 #include <string>
 #include <vector>
 #include <cmath>
-//#include <Python.h>
 
 int main(int argc, char *argv[]){
   if(argc!=2){
@@ -12,7 +11,7 @@ int main(int argc, char *argv[]){
     return 0;
   }
   std::string plasma_data_file;
-  double mi, Te, B0, ne, E0, R0, LB, Ln, De0, vi0, kz, kx, my_max, my_min;
+  double mi, Te, B0, n0, E0, R0, LB, Ln, De0, vix, kz, kx, my_max, my_min;
   double voltage_scale[2];
   double voltage_increment, my_increment;
   unsigned int N_voltages, N_modes;
@@ -46,12 +45,12 @@ int main(int argc, char *argv[]){
 	  f >> LB;
 	else if (name == "Ln")
 	  f >> Ln;
-    else if (name == "ne")
-	  f >> ne;
+    else if (name == "n0")
+	  f >> n0;
 	else if (name == "electron_diffusion")
 	  f >> De0;
-	else if (name == "vi0")
-	  f >> vi0;
+	else if (name == "vix")
+	  f >> vix;
 	else if (name == "lambda_z"){
 	  f >> kz;
 	  kz=2*M_PI/kz;
@@ -75,7 +74,7 @@ int main(int argc, char *argv[]){
       my_increment = (my_max - my_min) / (N_modes - 1);
       f.close();
   }
-
+  
   std::ofstream s("solution.txt");
   if(s.is_open()){
 	s << "###########################################\n";
@@ -94,11 +93,11 @@ int main(int argc, char *argv[]){
 	s << "###########################################\n";
 	s << "#             Output parameters           #\n";
 	s << "###########################################\n";	
-	s << "#  Voltage [V]   m decr []      f decr [Hz]   Ia decr [A]   m incr []     "	
-	      " f incr [Hz]   Ia decr [A]     Ion velocity [m/s]   De [m^2/s]    Te [eV]\n";
-    double E, T, cs, Ix, nupara, scale_factor, De, vd, vD, v0, vix; 
+	s << "#  Voltage [V]   m decr []      f decr [Hz]   vi1 decr [m/s]   Ia decr [A]   ne/n0 decr []  m incr []     "	
+	      " f incr [Hz]   vi1 incr [m/s]   Ia incr [A]   ne/n0 incr []   Ion velocity [m/s]   De [m^2/s]    Te [eV]\n";
+    double E, T, cs, Ix, nupara, scale_factor, De, vd, vD, v0, vi0, vi1, ne_over_n0; 
 	double wi0, my, ky, ky_incr, ky_decr, kp, wd, wD, w0, wR, wR_incr, wR_decr;
-	double  wI_new, wI_old, wI_previous;
+	double  wI_new, wI_old, wI_previous, wI_decr, wI_incr;
 
 	s.setf(std::ios::scientific, std::ios::floatfield);
     s.precision(4);
@@ -113,12 +112,15 @@ int main(int argc, char *argv[]){
       vd = -T/e/B0/Ln;
       vD = -2*T/e/B0/LB;
       v0 = -E/B0;
-      vix = vi0*sqrt(scale_factor);
-      wi0 = kx*vix;
+      vi0 = vix*sqrt(scale_factor);
+      wi0 = kx*vi0;
 
 	  wI_new = -1.0; wI_old = -1.0; wI_previous = -1.0;
 	  bool flag = false;
       for( my = my_min; my < my_max; my+=my_increment){
+		  
+		  //std::cout << wI_old << "   " << wI_previous << "   " << wI_new << std::endl;
+		  
 		  wI_old=wI_previous;
 	      wI_previous=wI_new;
 	      ky = my/R0;
@@ -145,6 +147,7 @@ int main(int argc, char *argv[]){
 
 			  s << "  " << E*Ln << "       " ;
 			  
+			  // DECREASING VOLTAGES
 			  ky_decr = floor(my)/R0;
 			  kp = sqrt(kx*kx+ky_decr*ky_decr);
 			  wd = vd*ky_decr;
@@ -155,19 +158,43 @@ int main(int argc, char *argv[]){
                    16*pow(cs,4)*pow(kp,4)/pow((wd-wD),2)*(pow((wi0-w0-wD),2)+nupara*nupara)+
                    8*pow(cs,6)*pow(kp,6)/pow((wd-wD),3)*(wi0-w0-wD))+
                    pow(cs,4)*pow(kp,4)/pow((wd-wD),2)+4*cs*cs*kp*kp/(wd-wD)*
-                   (wi0-w0-wD));	
+                   (wi0-w0-wD));
+				   
+			  wI_decr = 0.5/sqrt(2)*sqrt(sqrt(pow(cs,8)*pow(kp,8)/pow((wd-wD),4) +
+                   16*pow(cs,4)*pow(kp,4)/pow((wd-wD),2)*(pow((wi0-w0-wD),2)+nupara*nupara)+ 
+	               8*pow(cs,6)*pow(kp,6)/pow((wd-wD),3)*(wi0-w0-wD))-
+                   pow(cs,4)*pow(kp,4)/pow((wd-wD),2)-4*cs*cs*kp*kp/(wd-wD)* 
+	               (wi0-w0-wD));
 
 			  s << (int)floor(my) << "           " << wR_decr/M_PI/2;
 			  
 			  
 			  double dphi = E*Ln/10;//5;
-			  Ix = sqrt(pow((ky*ne*(wd-wD)*(wI_previous+kz*kz*De)*e*e*dphi*dphi/T/B0)/
-			              (pow(wR-w0-wD,2)+pow(wI_previous+kz*kz*De,2)),2) +
-				          pow((ky*ne*(wd-wD)*(wR-w0-wD)*e*e*dphi*dphi/T/B0)/
-			              (pow(wR-w0-wD,2)+pow(wI_previous+kz*kz*De,2)),2))*M_PI*R0*R0/8;
-			  s << "    " << Ix ;
-			 
 			  
+			  //fact=(n0*(wd-wD)*e*e*dphi/T)/(pow(wR-w0-wD,2)+pow(wI_previous+kz*kz*De,2))
+			  //vire= vi0*(wR_decr)
+			  
+			  // Ion velocity fluctuation
+			  
+			  vi1 = dphi*(e/mi*kx*(wR_decr-kx*vi0))/(pow(wR_decr-kx*vi0,2)+wI_decr*wI_decr)*sqrt(1+wI_decr*wI_decr/pow(wR_decr-kx*vi0,2));
+			  
+			  s << "    " << vi1 ;
+			  
+			  // Current fluctuation
+			  Ix = sqrt(pow((ky*n0*(wd-wD)*(wI_decr+kz*kz*De)*e*e*dphi*dphi/T/B0)/
+			              (pow(wR_decr-w0-wD,2)+pow(wI_decr+kz*kz*De,2)) ,2) +
+				          pow((ky*n0*(wd-wD)*(wR_decr-w0-wD)*e*e*dphi*dphi/T/B0)/
+			              (pow(wR_decr-w0-wD,2)+pow(wI_decr+kz*kz*De,2)),2))*M_PI*R0*R0/8;//*2-7.0/1000;
+			  
+			  s << "      " << Ix ;
+			 
+			  // density flucuation
+			  ne_over_n0 = sqrt(pow(((wd-wD)*e*dphi/T*(wR_decr-w0-wD))/(pow((wR_decr-w0-wD),2)+pow(wI_decr+kz*kz*De,2)),2) + 
+			               pow(((wd-wD)*e*dphi/T*(wI_decr+kz*kz*De))/(pow((wR_decr-w0-wD),2)+pow(wI_decr+kz*kz*De,2)),2));
+			  
+			  s << "    " << ne_over_n0 ;
+			  
+			  // INCREASING VOLTAGES
 			  ky_incr = ceil(my)/R0;
 			  kp = sqrt(kx*kx+ky_incr*ky_incr);
 			  wd = vd*ky_incr;
@@ -179,30 +206,40 @@ int main(int argc, char *argv[]){
                    8*pow(cs,6)*pow(kp,6)/pow((wd-wD),3)*(wi0-w0-wD))+
                    pow(cs,4)*pow(kp,4)/pow((wd-wD),2)+4*cs*cs*kp*kp/(wd-wD)*
                    (wi0-w0-wD));
+			  wI_incr = 0.5/sqrt(2)*sqrt(sqrt(pow(cs,8)*pow(kp,8)/pow((wd-wD),4) +
+                   16*pow(cs,4)*pow(kp,4)/pow((wd-wD),2)*(pow((wi0-w0-wD),2)+nupara*nupara)+ 
+	               8*pow(cs,6)*pow(kp,6)/pow((wd-wD),3)*(wi0-w0-wD))-
+                   pow(cs,4)*pow(kp,4)/pow((wd-wD),2)-4*cs*cs*kp*kp/(wd-wD)* 
+	               (wi0-w0-wD));
+			  s << "        " << (int)ceil(my)<< "           " << wR_incr/M_PI/2;
 			  
-			  s << "       " << (int)ceil(my)<< "           " << wR_incr/M_PI/2;
+			  // Ion velocity fluctuation increasing voltages 
+			  vi1 = dphi*(e/mi*kx*(wR_incr-kx*vi0))/(pow(wR_incr-kx*vi0,2)+wI_incr*wI_incr)*sqrt(1+wI_incr*wI_incr/pow(wR_incr-kx*vi0,2));
 			  
+			  s << "    " << vi1 ;
+			  
+			  // Current fluctuation increasing voltages 
+			  Ix = sqrt(pow((ky*n0*(wd-wD)*(wI_incr+kz*kz*De)*e*e*dphi*dphi/T/B0)/
+			              (pow(wR_incr-w0-wD,2)+pow(wI_incr+kz*kz*De,2)),2) +
+				          pow((ky*n0*(wd-wD)*(wR_incr-w0-wD)*e*e*dphi*dphi/T/B0)/
+			              (pow(wR_incr-w0-wD,2)+pow(wI_incr+kz*kz*De,2)),2))*M_PI*R0*R0/8;//*2-7.0/1000;
+		      s << "      " << Ix ;
 
-			  Ix = sqrt(pow((ky*ne*(wd-wD)*(wI_previous+kz*kz*De)*e*e*dphi*dphi/T/B0)/
-			              (pow(wR-w0-wD,2)+pow(wI_previous+kz*kz*De,2)),2) +
-				          pow((ky*ne*(wd-wD)*(wR-w0-wD)*e*e*dphi*dphi/T/B0)/
-			              (pow(wR-w0-wD,2)+pow(wI_previous+kz*kz*De,2)),2))*M_PI*R0*R0/8;
-		      s << "    " << Ix ;
-
+			  // density flucuation
+			  ne_over_n0 = sqrt(pow(((wd-wD)*e*dphi/T*(wR_incr-w0-wD))/(pow((wR_incr-w0-wD),2)+pow(wI_incr+kz*kz*De,2)),2) + 
+			               pow(((wd-wD)*e*dphi/T*(wI_incr+kz*kz*De))/(pow((wR_incr-w0-wD),2)+pow(wI_incr+kz*kz*De,2)),2));
 			  
+			  s << "    " << ne_over_n0 ;
 			  
 			  
 
 			  flag = true;
 
 			  
-			  s << "      " << vix << "          " << De << "    " << T/e << "\n"; 
+			  s << "      " << vi0 << "          " << De << "    " << T/e << "\n"; 
 			  
 
-	  
 	        }
-	
-	
 	
       }
       if (flag == false){
