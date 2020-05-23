41c6b480a1538a2944b3a04befd26c065d7f4b40
Andrea Marcovati <amarcova@stanford.edu>
2020-05-23 15:50:07 -0700

    implements biconj solver and validation

---
+++ nonlinearmodel.cpp
@@ -0,0 +1,183 @@
+#include <fstream>
+#include <complex>
+#include <iomanip>
+#include <vector>
+#include "nonlinearmodel.hpp"
+
+
+namespace mtl { 
+
+    template <>
+    struct Collection<A_phi>
+    {
+        typedef double value_type;
+        typedef int    size_type;
+    };
+
+    namespace ashape {
+        template <> struct ashape_aux<A_phi> 
+        {       typedef nonscal type;    };
+    }
+}
+
+NonlinearModel::NonlinearModel(double& mi_,  double& Te_, double& B0_, double& E0_ ,
+              double& R0_,  double& LB_, double& Ln_, double& De0_,
+              double& vix_, double& kz_, double& kx_, double& n0_,
+              double& t_, int& Ni_, int& Nj_, double& tf_,
+              double& n10_n0_): LinearModel(mi_, Te_, B0_, E0_, R0_, LB_, 
+              Ln_, De0_, vix_, kz_, kx_, n0_, t_), 
+              tf{tf_}, n0{n0_}, n10{n10_n0_*n0}, Ni{Ni_}, Nj{Nj_} {
+                
+                // initialize the memory for solutions
+                n1.resize(Ni+1, dense_vector<complex_number>(Nj+1));
+                vx1.resize(Ni+1, dense_vector<complex_number>(Nj+1));
+                vy1.resize(Ni+1, dense_vector<complex_number>(Nj+1));
+                phi1.resize(Ni+1, dense_vector<complex_number>(Nj+1));
+                
+                // initialize temporary containers
+                sigma_phi.resize(Nj+1);
+                r_phi.resize(Nj+1);
+                
+                Z0 = n0/B0/Ln - 2.0*n0/B0/LB; Q0 = E0/B0 + 2.0*Te/B0/LB;
+                t = 0; // simulation time initialized to 0
+                dy = 2*M_PI*R0/Nj;
+                dt = tf/Ni;
+                b = dense_vector<complex_number>(spacesteps_number());
+                BuildInitialConditions();
+                
+              } 
+void NonlinearModel::BuildInitialConditions(){
+    double y;
+    for( int j = 0; j < Nj+1; j++){
+      y = dy*(1.0-1.0/Nj)*j;
+      n1[0][j] = n10 * ( cos(my_decr/R0*y) +i1*sin(my_decr/R0*y) );
+      vx1[0][j] = dvxi1_decr(n10/n0)  * ( cos(my_decr/R0*y) +i1*sin(my_decr/R0*y) );
+      vy1[0][j] = dvyi1_decr(n10/n0)  * ( cos(my_decr/R0*y) +i1*sin(my_decr/R0*y) );
+      phi1[1][j] = dphi_decr(n10/n0)  * ( cos(my_decr/R0*y) +i1*sin(my_decr/R0*y) );
+
+    }
+  }
+  
+  void NonlinearModel::solve(){
+    int i = 0;
+    solve_phi1(i);
+    SaveSolution();
+  }
+
+  
+  complex_number NonlinearModel::lambda(int i, int j){
+    return fact_phi*i1*kx/B0*d_y(n1[i], j) / (Z0 + (i1*kx/B0 - 2.0/B0/LB)*n1[i][j] );
+
+  }
+  
+  complex_number NonlinearModel::g(int& i, int j){
+    return -( (Q0+fact_phi*vy1[i][j])*d_y(n1[i],j)+(n0+fact_phi*n1[i][j])*(i1*kx*vx1[i][j]+d_y(vy1[i],j))
+           + (vi0+fact_phi*vx1[i][j])*i1*kx*n1[i][j]-De0*kz*kz*n1[i][j]+vx1[i][j]*n0/Ln ) /
+           ( Z0 + ( fact_phi*i1*kx/B0 - 2.0/B0/LB )*n1[i][j] );
+  }  
+
+  void NonlinearModel::build_b(int& i){
+    for(int j = 0; j < spacesteps_number(); j++){
+                  b[j] = 12.0*dy* g(i,j);
+        }  
+  }
+
+  void NonlinearModel::solve_phi1(int& i){
+    
+    
+    A_phi A(this,i);
+    build_b(i);                       
+    pc::identity<A_phi>        P(A);  
+    //basic_iteration<double> iter{r0, 1000, 1.e-5};  
+    r0 = dense_vector<complex_number>(spacesteps_number(),1.0);    
+    noisy_iteration<double> iter{r0, 1000, 1.e-5};
+    bicgstab(A, phi1[i], b, P, iter);
+             
+  }
+  
+  void NonlinearModel::solver_validation_t0(){
+    // linearize potential equation
+    
+    solution_container space(1,dense_vector<complex_number>(Nj+1));
+    solution_container exact_linear(1,dense_vector<complex_number>(Nj+1));
+    solution_container solution_linear(1,dense_vector<complex_number>(Nj+1));
+    solution_container solution_nonlinear(1,dense_vector<complex_number>(Nj+1));
+    
+    std::ofstream space_file("space.txt");
+    std::ofstream exact_linear_file("exact_linear.txt");
+    std::ofstream solution_linear_file("solution_linear.txt");
+    std::ofstream solution_nonlinear_file("solution_nonlinear.txt");
+    
+    // compute exact solution from linear analysis
+    double y;
+    for( int j = 0; j < Nj+1; j++){
+      y = dy*(1.0-1.0/Nj)*j;
+      space[0][j] = y;
+      exact_linear[0][j] = dphi_decr(n10/n0)  * ( cos(my_decr/R0*y) +i1*sin(my_decr/R0*y) );
+    }
+    
+    // compute linear solution using solver
+    fact_phi = 0.0; // setting nonlinear terms to 0
+    int i = 0; // first timestep (t = 0)
+    A_phi A(this,i);
+    build_b(i);                       
+    pc::identity<A_phi>        P(A);   
+    r0 = dense_vector<complex_number>(spacesteps_number(),1.0);
+    noisy_iteration<double> iter_lin{r0, 1000, 1.e-6};
+    bicgstab(A, solution_linear[0], b, P, iter_lin);
+    
+    // compute nonlinear solution using solver
+    fact_phi = 1.0; // turning on nonlinear terms
+    r0 = dense_vector<complex_number>(spacesteps_number(),1.0);
+    noisy_iteration<double> iter_nonlin{r0, 1000, 1.e-6};
+    build_b(i);                          
+    bicgstab(A, solution_nonlinear[0], b, P, iter_nonlin);    
+    
+    // save solutions
+    
+    Save2File(space,space_file);
+    Save2File(exact_linear,exact_linear_file);
+    Save2File(solution_linear,solution_linear_file);
+    Save2File(solution_nonlinear,solution_nonlinear_file);
+    
+    
+  }
+
+  void NonlinearModel::SaveSolution(){
+   std::ofstream s_phi1("phi1.txt");
+   //std::ofstream s_n1("n1.txt");
+   Save2File(phi1,s_phi1);
+  }  
+  
+  void NonlinearModel::Save2File(solution_container& v, std::ofstream& file){
+    unsigned int width{30};
+    file.setf(std::ios::scientific, std::ios::floatfield);
+    file.precision(17); 
+    for (unsigned int i = 0; i< v.size(); i++){
+      for (unsigned int j = 0; j < size(v[0]); j++){
+        file << v[i][j].real() << std::setw(width);
+      }
+      file << "\n";
+    }
+    file.close(); 
+  }
+
+  int NonlinearModel::spacesteps_number() const{ return Nj + 1;}
+  int NonlinearModel::timesteps_number() const{ return Ni + 1;}
+  
+  int NonlinearModel::index(int j){
+    if (j < 0)
+      return Nj + 1 + j;
+    else if (j > Nj)
+      return j - Nj - 1;
+    else
+      return j;
+  }
+  
+  // compute and return space derivative of variable f
+  complex_number NonlinearModel::d_y(const dense_vector<complex_number>& f, int& j){
+
+    return (f[index(j-2)]-8.0*f[index(j-1)]+8.0*f[index(j+1)]-f[index(j+2)])/12.0/dy;
+  }
+  
+
--- nonlinearmodel.hpp
+++ nonlinearmodel.hpp
@@ -1,58 +1,136 @@
-#include <fstream>
+#pragma once
+#include <boost/numeric/mtl/mtl.hpp>
+#include <boost/numeric/itl/itl.hpp>
 #include <complex>
-#include <iomanip>
 #include <vector>
+#include "linearmodel.hpp"
+//#include "matrix_phi_OLDD.hpp"
+
+using namespace mtl;
+using namespace itl;
+
+using complex_number = std::complex<double>;
+using solution_container = std::vector<dense_vector<complex_number>>;
 
 class NonlinearModel: public LinearModel {
+  
   public:
+  
   NonlinearModel(double& mi_,  double& Te_, double& B0_, double& E0_ ,
               double& R0_,  double& LB_, double& Ln_, double& De0_,
               double& vix_, double& kz_, double& kx_, double& n0_,
-              double& t_, unsigned& Ni_, unsigned& Nj_, double& tf_,
-              double& n10_n0_, unsigned& m_n10_) 
-              : LinearModel(mi_, Te_, B0_, E0_, R0_, LB_, 
-              Ln_, De0_, vix_, kz_, kx_, n0_, t_), 
-              tf{tf_}, n10{n10_n0_*n0}, Ni{Ni_}, Nj{Nj_}, m_n10{m_n10_}{
-                
-                // initialize the memory for solutions
-                n1.resize(Ni, std::vector<std::complex<double>>(Nj));
-                vx1.resize(Ni, std::vector<std::complex<double>>(Nj));
-                vy1.resize(Ni, std::vector<std::complex<double>>(Nj));
-                phi1.resize(Ni, std::vector<std::complex<double>>(Nj));
-                sigma_phi.resize(Nj);
-                r_phi.resize(Nj);
-                
-                Q0 = n0/B0/Ln; Z0 = E0/B0 + 2*Te/B0/LB;
-                t = 0; // simulation time initialized to 0
-                dy = 2*M_PI*R0/(Nj - 1);
-                dt = tf/(Ni - 1);
-                
-                BuildInitialConditions();
-                
-                for (auto it = n1[0].begin(); it != n1[0].end(); ++it)
-                  std::cout << (*it).real() << std::endl;                
-                
-                //std::cout << "dy = " << dy << "; dt =" << dt <<std::endl;
-/*              print to check
-                for (auto it = n1.begin(); it != n1.end(); ++it){
-                  for (auto it2 = (*it).begin(); it2 != (*it).end(); ++it2)
-                    std::cout << (*it2).real() << " ";
-                  std::cout << std::endl;
-                }*/
-              } 
-  void BuildInitialConditions(){
-    for( unsigned j = 0; j < Nj; j++){
-      double y = dy*j;
-      n1[0][j].real( n10 * sin(m_n10/R0*y) );
-    }
-  }
-
-  private:
+              double& t_, int& Ni_, int& Nj_, double& tf_,
+              double& n10_n0_);
+               
+  void solve();
   
+  void solver_validation_t0();
+  
+  private:
+
+  void BuildInitialConditions();
+  
+  complex_number lambda(int i, int j);
+  
+  complex_number g(int& i, int j);
+  
+  void build_b(int& i);
+
+  void solve_phi1(int& i);
+  
+  void SaveSolution();
+  
+  void Save2File(solution_container& f, std::ofstream& file);
+
+  int spacesteps_number() const;
+  int timesteps_number() const;
+  
+  int index(int j);
+  
+  // compute and return space derivative of variable f
+  complex_number d_y(const dense_vector<complex_number>& f, int& j);
+  
+  private:
+  friend class A_phi;
   double tf, t, dt, dy;
-  double n10;
+  double n0, n10;
   double Q0, Z0;
-  unsigned Ni, Nj, m_n10;
-  std::vector<std::vector<std::complex<double>>> n1, vx1, vy1, phi1;
-  std::vector<std::complex<double>> sigma_phi, r_phi;
+  int i, Ni, Nj;
+  solution_container n1, vx1, vy1, phi1;
+  std::vector<complex_number> sigma_phi, r_phi;
+  
+  dense_vector<complex_number> b;
+  dense_vector<complex_number> r0;
+  //A_phi* A;
+ 
+  double fact_phi{1.0};
+  
 };
+
+
+
+class A_phi{
+  
+  public:
+  
+  // default constructor
+  A_phi(){
+    ptr = nullptr;
+    i = -1; 
+  }
+  
+  A_phi(class NonlinearModel* ptr_, int i_):ptr{ptr_}, i{i_}{}
+  
+
+  
+  /** Helper function to perform multiplication . Allows for delayed
+  * evaluation of results .
+  * Assign::apply(a, b) resolves to an assignment operation such as
+  * a += b, a -= b, or a = b.
+  * @pre @a size (v) == size (w) */
+  
+  template <typename VectorIn , typename VectorOut , typename Assign >
+  void mult ( const VectorIn& v, VectorOut& w, Assign ) const{
+    //double temp = 0.0;
+    
+    for(int j = 0; j < number_rows(); j++){
+        Assign::apply(w[j],                        v[ptr->index(j-2)]
+                                             - 8.0*v[ptr->index(j-1)]
+               - (ptr->dy)*12.0*(ptr->lambda(i,j))*v[j] 
+                                             + 8.0*v[ptr->index(j+1)]  
+                                                -  v[ptr->index(j+2)]); 
+    }
+    
+    
+    }
+  
+  /** Matvec forwards to MTL 's lazy mat_cvec_multiplier operator */
+  template <typename Vector>
+  mtl::vec::mat_cvec_multiplier<A_phi, Vector>
+  operator *( const Vector & v) const{
+    return mtl::vec::mat_cvec_multiplier<A_phi, Vector>(*this,v); 
+  }
+  
+  void print_address(){
+    std::cout << (*this).number_rows() << std::endl;
+    
+  }
+  int number_rows() const{return ptr->spacesteps_number();}
+  int number_columns() const{return ptr->spacesteps_number();}
+  int size() const{return number_rows()*number_columns();}
+  
+  private:
+  NonlinearModel* ptr;
+  int i;
+};
+
+
+/** The number of elements in the matrix . */
+inline std::size_t size (const A_phi & A){ return A.size();}
+
+/** The number of rows in the matrix . */
+inline std::size_t num_rows (const A_phi & A){ return A.number_rows();}
+
+
+/** The number of columns in the matrix . */
+inline std::size_t num_cols (const A_phi & A){ return A.number_columns();}
