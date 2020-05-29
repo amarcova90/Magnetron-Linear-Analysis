6627ab404dbe451f64182ef5a9cbd792b29db819
Andrea Marcovati <amarcova@stanford.edu>
2020-05-28 21:21:54 -0700

    performs validation of nonlinear solver

--- main.cpp
+++ main.cpp
@@ -34,7 +34,7 @@ int main(int argc, char *argv[]){
                     my_increment, N_voltages, N_modes, ne_over_n0, 
                     Ni, Nj, tf, n10_n0);
   
-  std::ofstream s("solution.txt");  
+  std::ofstream s("linear_model_solutions/solution.txt");  
   PrintInputHeader(s);
   PrintInputData(s,mi,Te,B0,n0,R0,t,LB,Ln,kz,kx,t,ne_over_n0,N_voltages,N_modes);
   PrintOutputHeader(s);
@@ -45,7 +45,7 @@ int main(int argc, char *argv[]){
   NonlinearModel NLdischarge(mi, Te, B0, E0, R0, LB, Ln, De0, vix, kz, kx, n0, t,
                              Ni, Nj, tf, n10_n0);
   
-  NLdischarge.solver_validation_t0();
+  
   //NLdischarge.solve();
   for(unsigned int i = 0; i < N_voltages; i++){
     E = E0*(voltage_scale.first+voltage_increment*i);
@@ -53,18 +53,18 @@ int main(int argc, char *argv[]){
     discharge.Save2File(s,ne_over_n0);
   }
    
-  std::ofstream g("w_solution.txt");
+  std::ofstream g("linear_model_solutions/w_solution.txt");
 
   // Read data from file
   if (g.is_open()){
-
+    
     g.setf(std::ios::scientific, std::ios::floatfield);
     g.precision(4);
     discharge.set_E(E0);
 
     for( double my = 2.0; my < my_max; my+=my_increment){
     double ky = my/R0;
-    
+
     g  << my << "   ";	
     g  << discharge.wR(ky) << "   ";	
     g  << discharge.A() << "   ";	
@@ -76,7 +76,18 @@ int main(int argc, char *argv[]){
     g  << discharge.gamma(ky) << "\n";	      
 
     }
+    g.close();
   }
+  
+  // nonlinear m odel validation;
+  int Nj_val = 2000000;
+  NonlinearModel NLdischarge_val(mi, Te, B0, E0, R0, LB, Ln, De0, vix, kz, kx, n0, t,
+                             Ni, Nj_val, tf, n10_n0);
+  for (int N_val = 1000; N_val < Nj_val; N_val*=2){
+    NLdischarge_val.resize(1,N_val);
+    NLdischarge_val.solver_validation();
+  }
+
   return 0;
 }
   
