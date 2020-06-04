8e82d7876c7de77b0fea24becfbf783ea575cfc7
Andrea Marcovati <amarcova@stanford.edu>
2020-06-04 15:30:15 -0700

    fixed error message

--- main_NLvalidation.cpp
+++ main_NLvalidation.cpp
@@ -10,7 +10,7 @@ int main(int argc, char *argv[]){
   // Argument check:
   if(argc!=2){
     std::cout << "Wrong number of inputs. Usage";
-    std::cout << "$ ./main plasma_data_file" << std::endl;
+    std::cout << "$ ./run_NLvalidation plasma_data_file" << std::endl;
     return 0;
   }
   // Variable definition:
@@ -28,17 +28,20 @@ int main(int argc, char *argv[]){
   plasma_data_file = argv[1];
   std::ifstream f(plasma_data_file);
 
-
   ReadNonlinearDatafromFile(f, mi, Te, B0, n0, E0, R0, LB, Ln, De0, vix, kz, kx, 
                     my_max, my_min, t, voltage_scale, voltage_increment, 
                     my_increment, N_voltages, N_modes, ne_over_n0, 
                     Ni, Nj, tf, n10_n0);
 
+  std::ofstream s("validation_files/input_data.txt");  
   
-  int Nj_val = 2000000;
+  PrintNonlinearInputHeader(s);
+  PrintNonlinearInputData(s,mi,Te,B0,n0,R0,t,LB,Ln,kz,kx,t,ne_over_n0,N_voltages,N_modes,Ni,Nj,tf,n10_n0);
+
+  int Nj_val = 500000;
   NonlinearModel NLdischarge_val(mi, Te, B0, E0, R0, LB, Ln, De0, vix, kz, kx, n0, t,
                              Ni, Nj_val, tf, n10_n0);
-  for (int N_val = 1000; N_val < Nj_val; N_val*=2){
+  for (int N_val = 100; N_val < Nj_val; N_val*=2){
     NLdischarge_val.resize(1,N_val);
     NLdischarge_val.solver_validation();
   }
