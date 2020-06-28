b65423437ec156d244fab67af0eacd6d11e131ef
Andrea Marcovati <amarcova@stanford.edu>
2020-06-28 15:02:25 -0700

    solves nonlinear equations

---
+++ main_nonlinear.cpp
@@ -0,0 +1,47 @@
+#include <fstream>
+#include <iostream>
+#include <cmath>
+#include "util.hpp"
+#include "linearmodel.hpp"
+#include "nonlinearmodel.hpp"
+
+
+int main(int argc, char *argv[]){
+  // Argument check:
+  if(argc!=2){
+    std::cout << "Wrong number of inputs. Usage";
+    std::cout << "$ ./run_NLvalidation plasma_data_file" << std::endl;
+    return 0;
+  }
+  // Variable definition:
+  std::string plasma_data_file;
+  double mi, t, Te, B0, n0, E0, R0, LB, Ln, De0, vix, kz, kx, 
+         my_max, my_min, ne_over_n0;
+  std::pair<double,double> voltage_scale;
+  double voltage_increment, my_increment;
+  unsigned int N_voltages, N_modes;
+  
+  // Additional arguments for nonlinear analysis
+  int Ni, Nj;
+  double tf, n10_n0;
+  
+  plasma_data_file = argv[1];
+  std::ifstream f(plasma_data_file);
+
+  ReadNonlinearDatafromFile(f, mi, Te, B0, n0, E0, R0, LB, Ln, De0, vix, kz, kx, 
+                    my_max, my_min, t, voltage_scale, voltage_increment, 
+                    my_increment, N_voltages, N_modes, ne_over_n0, 
+                    Ni, Nj, tf, n10_n0);
+
+  //std::ofstream s("validation_files/input_data.txt");  
+  
+  //PrintNonlinearInputHeader(s);
+  //PrintNonlinearInputData(s,mi,Te,B0,n0,R0,t,LB,Ln,kz,kx,t,ne_over_n0,N_voltages,N_modes,Ni,Nj,tf,n10_n0);
+
+  NonlinearModel NLdischarge_val(mi, Te, B0, E0, R0, LB, Ln, De0, vix, kz, kx, n0, t,
+                             Ni, Nj, tf, n10_n0);
+ 
+  NLdischarge_val.solve();
+  return 0;
+}
+  
