87e648c00ead6b35e20397ed69bcc53515b6efd9
Andrea Marcovati <amarcova@stanford.edu>
2020-08-30 10:15:15 -0700

    reformulated eqs with velocity potential and added cross-B diffusion

--- nonlinearmodel.cpp
+++ nonlinearmodel.cpp
@@ -20,6 +20,20 @@ namespace mtl{
   }
 }
 
+namespace mtl{
+  template <>
+  struct Collection<A_chi>
+  {
+      typedef double value_type;
+      typedef int    size_type;
+  };
+
+  namespace ashape {
+      template <> struct ashape_aux<A_chi> 
+      {       typedef nonscal type;    };
+  }
+}
+
 NonlinearModel::NonlinearModel(double& mi_,  double& Te_, double& B0_, double& E0_ ,
               double& R0_,  double& LB_, double& Ln_, double& De0_,
               double& vix_, double& kz_, double& kx_, double& n0_,
@@ -31,6 +45,12 @@ NonlinearModel::NonlinearModel(double& mi_,  double& Te_, double& B0_, double& E
               // initialize the memory for solutions
               resize(Ni_,Nj_);              
               Z0 = n0/B0/Ln - 2.0*n0/B0/LB; Q0 = E0/B0 + 2.0*Te/B0/LB;
+              wce = e*B0/me;
+              nu = 0.0*1.0e5;  
+              rL2 = Te/B0/wce;
+              std::cout << "wce = " << wce << std::endl; 
+              std::cout << "rL2 = " << rL2 << std::endl; 
+              std::cout << "nupara = " << nupara << std::endl; 
 }
 
 void NonlinearModel::BuildInitialConditions(){
@@ -41,11 +61,35 @@ void NonlinearModel::BuildInitialConditions(){
     n1[0][j]   = n10                 * ( cos(my_decr/R0*y) +i1*sin(my_decr/R0*y) );
     vx1[0][j]  = dvxi1_decr(n10/n0)  * ( cos(my_decr/R0*y) +i1*sin(my_decr/R0*y) );
     vy1[0][j]  = dvyi1_decr(n10/n0)  * ( cos(my_decr/R0*y) +i1*sin(my_decr/R0*y) );
+    vz10[j]    = dvzi1_decr(n10/n0)  * ( cos(my_decr/R0*y) +i1*sin(my_decr/R0*y) );
   }
+  
+  for( int j = 0; j < Nj+1; j++){
+    y = dy*j;
+    lap1[0][j]    = - i1*kx*vx1[0][j] - d_y(vy1[0],j) - i1*kz*vz10[j];
+  }
+  
+/*   dense_vector<complex_number> test3(Nj+1);
+   */
+  
+/*   double y1{-dy};
+  
+  for( int j = 0; j < Nj+1; j++){
+    y1 += dy;
+    y = dy*j;
+    std::cout << "y = " << y << "; y1 = " << y1 << std::endl;
+    std::cout << "y^4 = " << y*y*y*y << "; y1 = " << y1*y1*y1*y1 << std::endl;
+    test3[j]    = cos(y) ;
+  }  
+  
+  for( int j = 10; j < 20; j++){
+    std::cout << "d4 = "<< d4_y(test3,j) << std::endl;
+  }  */
 
 }
   
 void NonlinearModel::solve(){
+  solve_chi1(0);
   solve_phi1(0);
   for(int i = 1; i < Ni+1; i++){
     march_RK4(i);
@@ -59,16 +103,38 @@ complex_number NonlinearModel::lambda(int i, int j){
   return fact_lin*i1*kx/B0*d_y(n1[i], j) / (Z0 + (i1*kx/B0 - 2.0/B0/LB)*n1[i][j] );
 
 }
-  
+
+/* complex_number NonlinearModel::g(int& i, int j){
+  return -( (Q0-fact_lin*d_y(chi1[i],j))*d_y(n1[i],j) + i1*kx*vi0*n1[i][j] +
+            (kx*kx + kz*kz)*chi1[i][j]*n1[i][j] - i1*n0/Ln*kx*chi1[i][j] - 
+            nupara*n1[i][j] - (n0 + n1[i][j])*lap1[i][j]  ) /
+         ( Z0 + fact_lin*( i1*kx/B0 - 2.0/B0/LB )*n1[i][j] );
+} */
+
 complex_number NonlinearModel::g(int& i, int j){
-  return -( (Q0+fact_lin*vy1[i][j])*d_y(n1[i],j)+(n0+fact_lin*n1[i][j])*(i1*kx*vx1[i][j]+d_y(vy1[i],j))
-         + (vi0+fact_lin*vx1[i][j])*i1*kx*n1[i][j]-nupara*n1[i][j]+vx1[i][j]*n0/Ln ) /
+  return -( (Q0-fact_lin*d_y(chi1[i],j))*d_y(n1[i],j) + i1*kx*vi0*n1[i][j] +
+            (kx*kx )*chi1[i][j]*n1[i][j] - i1*n0/Ln*kx*chi1[i][j] - 
+            nupara*n1[i][j] - (n0 + n1[i][j])*(d2_y(chi1[i],j)-kx*kx*chi1[i][j]) ) /
          ( Z0 + fact_lin*( i1*kx/B0 - 2.0/B0/LB )*n1[i][j] );
 }
+  
+/* complex_number NonlinearModel::g(int& i, int j){
+  return -( (Q0+fact_lin*vy1[i][j])*d_y(n1[i],j) + (n0+fact_lin*n1[i][j])*(i1*kx*vx1[i][j]
+              + d_y(vy1[i],j))  + nu*rL2*d2_y(n1[i],j)
+            + (vi0+fact_lin*vx1[i][j])*i1*kx*n1[i][j] - (nupara + 0.0*nu*rL2*kx*kx)*n1[i][j] 
+            + vx1[i][j]*n0/Ln ) /
+         ( Z0 + fact_lin*( i1*kx/B0 - 2.0/B0/LB )*n1[i][j] ); 
+} */
 
 void NonlinearModel::build_b(int& i){
   for(int j = 0; j < spacesteps_number(); j++){
-                b[j] = g(i,j);
+                b[j] = 12.0*dy*g(i,j);
+      }
+}
+
+void NonlinearModel::build_b_chi(int& i){
+  for(int j = 0; j < spacesteps_number(); j++){
+                b_chi[j] = 12.0*dy*dy*lap1[i][j];
       }
 }
 
@@ -83,16 +149,37 @@ void NonlinearModel::solve_phi1(int i){
     r0 = b - A*phi1[i-1];
   }
   
-  basic_iteration<double> iter{r0, 1000, err_rel, err_abs};    
+  basic_iteration<double> iter{r0, Nj, err_rel, err_abs};    
   //noisy_iteration<double> iter{r0, 1000, err_rel, err_abs};
   bicgstab(A, phi1[i], b, P, iter);
 
 }
+
+void NonlinearModel::solve_chi1(int i){
+  A_chi A_c(this);
+  build_b_chi(i);                       
+  pc::identity<A_chi>        P(A_c);  
+  
+  if (i == 0){
+    r0 = dense_vector<complex_number>(spacesteps_number(),0.0);
+  }else{
+    r0 = b_chi - A_c*chi1[i-1];
+  }
+    
+  basic_iteration<double> iter{r0, Nj, err_rel, err_abs};
+  bicgstab(A_c, chi1[i], b_chi, P, iter);
+
+}
   
 complex_number NonlinearModel::f_n1(int i, int j){
-  return  (Q0 - fact_lin* i1*kx/B0*phi1[i][j])*d_y(n1[i],j) + 
+/*   return  (Q0 - fact_lin* i1*kx/B0*phi1[i][j])*d_y(n1[i],j) + 
           (Z0 + fact_lin* i1*kx/B0*n1[i][j] - fact_lin* 2*n1[i][j]/B0/LB)*d_y(phi1[i],j) -
-           nupara*n1[i][j] ;
+           nupara*n1[i][j] ; */
+  return  (Q0 - fact_lin* i1*kx/B0*phi1[i][j])*d_y(n1[i],j) + 
+          (Z0 + fact_lin* i1*kx/B0*n1[i][j] - fact_lin* 2.0*n1[i][j]/B0/LB)*d_y(phi1[i],j)
+          - nu*n0*rL2/Te*d2_y(phi1[i],j) + nu*n0*rL2/Te*kx*kx*phi1[i][j]
+          + nu*rL2*d2_y(n1[i],j) - (nupara+0.0*nu*rL2*kx*kx)*n1[i][j] -
+          D_*(kx*kx*kx*kx + kz*kz*kz*kz)*n1[i][j] - D_*d4_y(n1[i],j);
 }
 
 complex_number NonlinearModel::f_vx1(int i, int j){
@@ -105,6 +192,15 @@ complex_number NonlinearModel::f_vy1(int i, int j){
            e/mi*d_y(phi1[i],j);               
 }  
 
+complex_number NonlinearModel::f_lap1(int i, int j){
+  return  -vi0*i1*kx*lap1[i][j] + e/mi*( d2_y(phi1[i],j) - (kx*kx + kz*kz)*phi1[i][j] ) + 
+           d2_y(chi1[i],j)*d2_y(chi1[i],j) + d_y(chi1[i],j)*d3_y(chi1[i],j) - 
+           3.0*(kx*kx + kz*kz)*d_y(chi1[i],j)*d_y(chi1[i],j) - 
+           (kx*kx + kz*kz)*chi1[i][j]*d2_y(chi1[i],j) + 
+           2.0*(kx*kx + kz*kz)*(kx*kx + kz*kz)*chi1[i][j]*chi1[i][j] -
+           D_*(kx*kx*kx*kx + kz*kz*kz*kz)*lap1[i][j] - D_*d4_y(lap1[i],j);               
+}  
+
 void NonlinearModel::march_RK4(int i){
   
   // Update k1
@@ -112,52 +208,62 @@ void NonlinearModel::march_RK4(int i){
     k1_n1[j] =  f_n1(i-1,j)  *dt;
     k1_vx1[j] = f_vx1(i-1,j) *dt;
     k1_vy1[j] = f_vy1(i-1,j)*dt;
+    k1_lap1[j] = f_lap1(i-1,j)*dt;
   }    
   
   for(int j = 0; j < spacesteps_number(); j++){
     n1[i][j] = n1[i-1][j] + 0.5*k1_n1[j]; // temp for k2
     vx1[i][j] = vx1[i-1][j] + 0.5*k1_vx1[j]; // temp for k2
     vy1[i][j] = vy1[i-1][j] + 0.5*k1_vy1[j]; // temp for k2 
+    lap1[i][j] = lap1[i-1][j] + 0.5*k1_lap1[j];
   }    
   
   // Update phi1 and place temporarely in i
+  solve_chi1(i);
   solve_phi1(i);     
   
   // Update k2
   for(int j = 0; j < spacesteps_number(); j++){
     k2_n1[j] =  f_n1(i,j)*dt;
     k2_vx1[j] = f_vx1(i,j)*dt;    
-    k2_vy1[j] = f_vy1(i,j)*dt;            
+    k2_vy1[j] = f_vy1(i,j)*dt;    
+    k2_lap1[j] = f_lap1(i,j)*dt;    
   }
 
   for(int j = 0; j < spacesteps_number(); j++){           
     n1[i][j] = n1[i-1][j] + 0.5*k2_n1[j]; // temp for k3 
     vx1[i][j] = vx1[i-1][j] + 0.5*k2_vx1[j]; // temp for k3 
     vy1[i][j] = vy1[i-1][j] + 0.5*k2_vy1[j]; // temp for k3 
+    lap1[i][j] = lap1[i-1][j] + 0.5*k2_lap1[j];
   }
   
   // Update phi1 and place temporarely in i
+  solve_chi1(i);
   solve_phi1(i);
   // Update k3
   for(int j = 0; j < spacesteps_number(); j++){
     k3_n1[j] = f_n1(i,j)*dt;
     k3_vx1[j] = f_vx1(i,j)*dt;
     k3_vy1[j] = f_vy1(i,j)*dt;
+    k3_lap1[j] = f_lap1(i,j)*dt;
   }
 
   for(int j = 0; j < spacesteps_number(); j++){  
     n1[i][j] = n1[i-1][j] + k3_n1[j]; // temp for k3 
     vx1[i][j] = vx1[i-1][j] + k3_vx1[j]; // temp  
     vy1[i][j] = vy1[i-1][j] + k3_vy1[j]; // temp for k4
+    lap1[i][j] = lap1[i-1][j] + k3_lap1[j];
   }
   
   // Update phi1 and place temporarely in i
+  solve_chi1(i);
   solve_phi1(i);
   
   for(int j = 0; j < spacesteps_number(); j++){
     k4_n1[j] = f_n1(i,j)*dt;
     k4_vx1[j] = f_vx1(i,j)*dt;
     k4_vy1[j] = f_vy1(i,j)*dt;
+    k4_lap1[j] = f_lap1(i,j)*dt;
   }
   
   // Compute next step
@@ -165,7 +271,9 @@ void NonlinearModel::march_RK4(int i){
     n1[i][j] =  n1[i-1][j]  + (k1_n1[j]  + 2*k2_n1[j]  + 2*k3_n1[j]  + k4_n1[j])/6;
     vx1[i][j] = vx1[i-1][j] + (k1_vx1[j] + 2*k2_vx1[j] + 2*k3_vx1[j] + k4_vx1[j])/6;
     vy1[i][j] = vy1[i-1][j] + (k1_vy1[j] + 2*k2_vy1[j] + 2*k3_vy1[j] + k4_vy1[j])/6;
+    lap1[i][j] = lap1[i-1][j] + (k1_lap1[j] + 2*k2_lap1[j] + 2*k3_lap1[j] + k4_lap1[j])/6;
    }    
+  solve_chi1(i);
   solve_phi1(i);
   
 }
@@ -176,6 +284,9 @@ void NonlinearModel::resize(int Ni_new,int Nj_new){
   n1.resize(Ni+1, dense_vector<complex_number>(Nj+1));
   vx1.resize(Ni+1, dense_vector<complex_number>(Nj+1));
   vy1.resize(Ni+1, dense_vector<complex_number>(Nj+1));
+  chi1.resize(Ni+1, dense_vector<complex_number>(Nj+1));
+  lap1.resize(Ni+1, dense_vector<complex_number>(Nj+1));
+  vz10.resize(Nj+1);
   phi1.resize(Ni+1, dense_vector<complex_number>(Nj+1));
   
   // initialize temporary containers
@@ -185,22 +296,27 @@ void NonlinearModel::resize(int Ni_new,int Nj_new){
   k1_n1.resize(Nj+1);
   k1_vx1.resize(Nj+1);
   k1_vy1.resize(Nj+1);
-
+  k1_lap1.resize(Nj+1);
+  
   k2_n1.resize(Nj+1);
   k2_vx1.resize(Nj+1);
   k2_vy1.resize(Nj+1);
+  k2_lap1.resize(Nj+1);
 
   k3_n1.resize(Nj+1);
   k3_vx1.resize(Nj+1);
   k3_vy1.resize(Nj+1);
+  k3_lap1.resize(Nj+1);
 
   k4_n1.resize(Nj+1);
   k4_vx1.resize(Nj+1);
   k4_vy1.resize(Nj+1);    
+  k4_lap1.resize(Nj+1);
 
   dy = 2*M_PI*R0/(Nj+1.0);
   dt = tf/Ni;
   b.change_dim(spacesteps_number());
+  b_chi.change_dim(spacesteps_number());
   r0.change_dim(spacesteps_number());
   BuildInitialConditions();
 }
@@ -319,6 +435,10 @@ void NonlinearModel::SaveSolution(){
  Save2File(vx1,s_vx1);  
  std::ofstream s_vy1("vy1.txt");
  Save2File(vy1,s_vy1);    
+ std::ofstream s_chi1("chi1.txt");
+ Save2File(chi1,s_chi1); 
+ std::ofstream s_lap1("lap1.txt");
+ Save2File(lap1,s_lap1); 
 }  
   
 void NonlinearModel::Save2File(solution_container& v, std::ofstream& file){
@@ -346,9 +466,42 @@ int NonlinearModel::index(int j){
     return j;
 }
   
-// compute and return space derivative of variable f
+// compute and return space derivatives of variable f
 complex_number NonlinearModel::d_y(const dense_vector<complex_number>& f, int& j){
   return (f[index(j-2)]-8.0*f[index(j-1)]+8.0*f[index(j+1)]-f[index(j+2)])/12.0/dy;
 }
+
+complex_number NonlinearModel::d2_y(const dense_vector<complex_number>& f, int& j){
+  return (-f[index(j+2)]+16.0*f[index(j+1)]-30.0*f[index(j)]+16.0*f[index(j-1)]-f[index(j-2)])/12.0/dy/dy;  
+}
+
+complex_number NonlinearModel::d3_y(const dense_vector<complex_number>& f, int& j){
+  return (-f[index(j+3)]+8.0*f[index(j+2)]-13.0*f[index(j+1)]+13.0*f[index(j-1)]-8.0*f[index(j-2)]+f[index(j-3)])/8.0/dy/dy/dy;  
+}
+
+complex_number NonlinearModel::d4_y(const dense_vector<complex_number>& f, int& j){
+  double dy4 = dy*dy*dy*dy; 
+  return (-f[index(j+3)]+12.0*f[index(j+2)]-39.0*f[index(j+1)]+56.0*f[index(j)]-39.0*f[index(j-1)]+12.0*f[index(j-2)]-f[index(j-3)])/6.0/dy4;  
+}  
+
+/* complex_number NonlinearModel::d4_y(const dense_vector<complex_number>& f, int& j){
+  std::complex<double> dy4 = 6.0*dy*dy*dy*dy;
+  std::complex<double> f_p3 =  -f[index(j+3)];
+  std::complex<double> f_p2 =  12.0*f[index(j+2)];
+  std::complex<double> f_p1 =  -39.0*f[index(j+1)];
+  std::complex<double> f_ =  56.0*f[index(j)];
+  std::complex<double> f_m1 =  -39.0*f[index(j-1)];
+  std::complex<double> f_m2 =  12.0*f[index(j-2)];
+  std::complex<double> f_m3 =  -f[index(j-3)];
+  //std::cout << (-f_p3+f_p2-f_p1+f_-f_m1+f_m2-f_m3) << std::endl;
+  //std::cout << dy4 << std::endl;
+  std::complex<double> out_long = (f_p3+f_p2+f_p1+f_+f_m1+f_m2+f_m3)/dy4;  
   
+  return (complex_number)out_long;
+}   */
+
+/* complex_number NonlinearModel::d4_y2(const dense_vector<complex_number>& f, int& j){
+  long double dy4 = dy*dy*dy*dy;
+  return (1.0L*(std::complex<long double>)f[index(j+2)]-4.0L*(std::complex<long double>)f[index(j+1)]+6.0L*(std::complex<long double>)f[index(j)]-4.0L*(std::complex<long double>)f[index(j-1)]+1.0L*(std::complex<long double>)f[index(j-2)])/dy4;  
+}   */
 
--- nonlinearmodel.hpp
+++ nonlinearmodel.hpp
@@ -39,13 +39,19 @@ class NonlinearModel: public LinearModel {
   
   complex_number f_vy1(int i, int j);
   
+  complex_number f_lap1(int i, int j);
+  
   complex_number lambda(int i, int j);
   
   complex_number g(int& i, int j);
   
   void build_b(int& i);
+  
+  void build_b_chi(int& i);
 
   void solve_phi1(int i);
+  
+  void solve_chi1(int i);
   
   void march_RK4(int i);
   
@@ -60,24 +66,34 @@ class NonlinearModel: public LinearModel {
   
   // compute and return space derivative of variable f
   complex_number d_y(const dense_vector<complex_number>& f, int& j);
+  complex_number d2_y(const dense_vector<complex_number>& f, int& j);
+  complex_number d3_y(const dense_vector<complex_number>& f, int& j);
+  complex_number d4_y(const dense_vector<complex_number>& f, int& j);
+  //complex_number d4_y2(const dense_vector<complex_number>& f, int& j);
   
   private:
   friend class A_phi;
+  friend class A_chi;
   double tf, dt, dy;
   double n0, n10;
   double Q0, Z0;
+  double wce, rL2, nu;
+  double D_ = 1.32e-9;
   double err_rel{1e-3};
   double err_abs{1e-6};
   int i, Ni, Nj;
-  solution_container n1, vx1, vy1, phi1;
+  solution_container n1, vx1, vy1, phi1, chi1, lap1;
+  
+  std::vector<complex_number> vz10;
+  
   std::vector<complex_number> sigma_phi, r_phi;
   
-  std::vector<complex_number> k1_n1, k1_vx1, k1_vy1;
-  std::vector<complex_number> k2_n1, k2_vx1, k2_vy1;
-  std::vector<complex_number> k3_n1, k3_vx1, k3_vy1;
-  std::vector<complex_number> k4_n1, k4_vx1, k4_vy1;
+  std::vector<complex_number> k1_n1, k1_vx1, k1_vy1, k1_lap1;
+  std::vector<complex_number> k2_n1, k2_vx1, k2_vy1, k2_lap1;
+  std::vector<complex_number> k3_n1, k3_vx1, k3_vy1, k3_lap1;
+  std::vector<complex_number> k4_n1, k4_vx1, k4_vy1, k4_lap1;
   
-  dense_vector<complex_number> b;
+  dense_vector<complex_number> b, b_chi;
   dense_vector<complex_number> r0;
   //A_phi* A;
  
@@ -122,11 +138,11 @@ class A_phi{
     } */
 
     for(int j = 0; j < number_rows(); j++){
-        Assign::apply(w[j],                        v[ptr->index(j-2)]/dy/12
-                                             - 8.0*v[ptr->index(j-1)]/dy/12
-               - (ptr->lambda(i,j))*v[j] 
-                                             + 8.0*v[ptr->index(j+1)]/dy/12
-                                                -  v[ptr->index(j+2)]/dy/12); 
+        Assign::apply(w[j],                        v[ptr->index(j-2)]
+                                             - 8.0*v[ptr->index(j-1)]
+               - 12.0*(ptr->lambda(i,j))*v[j]*dy
+                                             + 8.0*v[ptr->index(j+1)]
+                                                -  v[ptr->index(j+2)]); 
     }    
     
 /*     double dy = ptr->dy;
@@ -170,16 +186,87 @@ class A_phi{
   
   private:
   NonlinearModel* ptr;
-  int i;
+  int  i;
+};
+
+
+class A_chi{
+  
+  public:
+  
+  // default constructor
+  A_chi(){
+    ptr = nullptr;
+  }
+  
+  A_chi(class NonlinearModel* ptr_):ptr{ptr_}{}
+  
+
+  
+  /** Helper function to perform multiplication . Allows for delayed
+  * evaluation of results .
+  * Assign::apply(a, b) resolves to an assignment operation such as
+  * a += b, a -= b, or a = b.
+  * @pre @a size (v) == size (w) */
+  
+  template <typename VectorIn , typename VectorOut , typename Assign >
+  void mult ( const VectorIn& v, VectorOut& w, Assign ) const{
+    //double temp = 0.0;
+    double dy = ptr->dy;
+    double kx = ptr->kx;
+    double kz = ptr->kz;
+    
+      for(int j = 0; j < number_rows(); j++){
+          Assign::apply(w[j],                        -v[ptr->index(j-2)]
+                                               + 16.0*v[ptr->index(j-1)]
+                   - (30.0+12.0*dy*dy*(kx*kx+kz*kz))*v[j]
+                                               + 16.0*v[ptr->index(j+1)]
+                                                     -v[ptr->index(j+2)]); 
+      }    
+      
+    }
+  
+  /** Matvec forwards to MTL 's lazy mat_cvec_multiplier operator */
+  template <typename Vector>
+  mtl::vec::mat_cvec_multiplier<A_chi, Vector>
+  operator *( const Vector & v) const{
+    return mtl::vec::mat_cvec_multiplier<A_chi, Vector>(*this,v); 
+  }
+
+/*   dense_vector<double>  operator [](int j) const{
+    double dy = ptr->dy;
+    dense_vector<double> out(number_columns(),0.0);
+    out[ptr->index(j-2)] = -1.0;
+    out[ptr->index(j-1)] = 16.0;
+    out[ptr->index(j)] = - (30.0+12.0*dy*dy*(ptr->kx*ptr->kx+ptr->kz*ptr->kz));
+    out[ptr->index(j+1)] = 16.0;
+    out[ptr->index(j+2)] = -1.0;
+    
+    return out; 
+  } */
+  
+  void print_address(){
+    std::cout << (*this).number_rows() << std::endl;
+    
+  }
+  int number_rows() const{return ptr->spacesteps_number();}
+  int number_columns() const{return ptr->spacesteps_number();}
+  int size() const{return number_rows()*number_columns();}
+  
+  private:
+  NonlinearModel* ptr;
 };
 
 
 /** The number of elements in the matrix . */
-inline std::size_t size (const A_phi & A){ return A.size();}
+template <typename mat>
+inline std::size_t size (const mat & A){ return A.size();}
 
 /** The number of rows in the matrix . */
-inline std::size_t num_rows (const A_phi & A){ return A.number_rows();}
+template <typename mat>
+inline std::size_t num_rows (const mat & A){ return A.number_rows();}
 
 
 /** The number of columns in the matrix . */
-inline std::size_t num_cols (const A_phi & A){ return A.number_columns();}
+template <typename mat>
+inline std::size_t num_cols (const mat & A){ return A.number_columns();}
